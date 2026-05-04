ea3442126d2e7933bd7a5f988c2d0583e88a77c9
danylo <levchenkodanylo@gmail.com>
2026-05-05 01:10:55 +0300

    start

---
+++ MyCourseWork/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MyCourseWork.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ MyCourseWork/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace MyCourseWork.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
